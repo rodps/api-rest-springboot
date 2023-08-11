4300368cede6dbea3f676531606e99d2a1e2185a
Rodrigo <rodrigopds73@gmail.com>
2023-08-11 13:00:55 -0300

    add error handling

---
+++ src/main/java/med/voll/api/infra/TratadorDeErros.java
@@ -0,0 +1,14 @@
+package med.voll.api.infra;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class TratadorDeErros {
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<Object> tratarErro404() {
+        return ResponseEntity.notFound().build();
+    }
+}
